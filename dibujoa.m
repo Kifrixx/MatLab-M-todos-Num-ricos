6ab825be5aacacf4336dcde1ec8590a641c18437
Kifrixx <143884980+Kifrixx@users.noreply.github.com>
2023-10-24 15:46:39 +0200

    Add files via upload

--- dibujoa.m
+++ dibujoa.m
@@ -1,6 +1,4 @@
-x = 1:1:4;
-y = f(x)
-plot(x, y);
-xlabel('x');
-ylabel('y');
-title('Gráfico de y = 35 + atan(x-5) - exp(5-x)');
+x=0:0.05:5;
+
+y=polinomio1a(x);
+plot(x,y,x,tan(x))
--- f.m
+++ f.m
@@ -1,4 +1,10 @@
-%% MATLAB
-function y = f(x)
-    y = 35 + atan(x-5)-exp(5-x);%funcion f
-end
+pol = [ 1 ; 1 ; 1];
+x = [ 0; (pi/6) ;(pi)/3 ];
+y = [ 0 ; 0.5774; 1.732];
+i = 1;
+    while ( i < length(x) )
+    pol = [pol, x.^i];
+
+    i = i +1;
+    end
+ sol = inv(pol)*y
