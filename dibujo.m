7085cbfcdb71bd8a4e45cbb9f36218a4f9e5c21e
Kifrixx <143884980+Kifrixx@users.noreply.github.com>
2023-12-03 12:54:56 +0100

    Delete dibujo.m

--- dibujo.m
+++
@@ -1,8 +0,0 @@
-function dibujo(a, b, w, yexacta)
-    tam = length(w);
-    x = linspace(a, b, tam);
-    x2 = 0:0.1:2;
-    plot(x, w,"blue", x2, feval(yexacta, x2),"--")
-    legend("w(x)","y(x)")
-    title("Gráfica comparativa")
-end
